8c505529b0d19bd8c88c352b9d2e049671ca214e
Tom Wiesing <tkw01536@gmail.com>
2019-05-08 15:23:51 +0200

    More advanced mathwebsearch test

--- cmd/mwsquery/cmd/main_test.go
+++ cmd/mwsquery/cmd/main_test.go
@@ -10,6 +10,9 @@ func TestMain(m *testing.M) {
 	integrationtest.Main(m, "docker-compose-mwsquery.yml", nil, "http://localhost:8080/")
 }
 
+const MWSXMLAnyQuery = "<mws:qvar>x</mws:qvar>"
+const MWSXMLPlusQuery = "<m:apply><plus /><mws:qvar>x</mws:qvar><mws:qvar>y</mws:qvar></m:apply>"
+
 func TestMWSQuery(t *testing.T) {
 	integrationtest.MarkSkippable(t)
 
@@ -19,13 +22,21 @@ func TestMWSQuery(t *testing.T) {
 		assetName string
 		wantErr   bool
 	}{
-		{"count all regular elements", &Args{Expressions: []string{"<mws:qvar>x</mws:qvar>"}, Count: true}, "testdata/count_all_regular.json", false},
-		{"first 10 regular elements", &Args{Expressions: []string{"<mws:qvar>x</mws:qvar>"}, From: 0, Size: 10}, "testdata/first_10_regular.json", false},
-		{"last 10 regular elements", &Args{Expressions: []string{"<mws:qvar>x</mws:qvar>"}, From: 2914, Size: 10}, "testdata/last_10_regular.json", false},
+		{"count all regular elements", &Args{Expressions: []string{MWSXMLAnyQuery}, Count: true}, "testdata/count_all_regular.json", false},
+		{"first 10 regular elements", &Args{Expressions: []string{MWSXMLAnyQuery}, From: 0, Size: 10}, "testdata/first_10_regular.json", false},
+		{"last 10 regular elements", &Args{Expressions: []string{MWSXMLAnyQuery}, From: 2914, Size: 10}, "testdata/last_10_regular.json", false},
 
-		{"count all mws ids", &Args{Expressions: []string{"<mws:qvar>x</mws:qvar>"}, Count: true, MWSIdsOnly: true}, "testdata/count_all_ids.json", false},
-		{"first 10 mws ids", &Args{Expressions: []string{"<mws:qvar>x</mws:qvar>"}, From: 0, Size: 10, MWSIdsOnly: true}, "testdata/first_10_ids.json", false},
-		{"last 10 mws ids", &Args{Expressions: []string{"<mws:qvar>x</mws:qvar>"}, From: 735, Size: 10, MWSIdsOnly: true}, "testdata/last_10_ids.json", false},
+		{"count all mws ids", &Args{Expressions: []string{MWSXMLAnyQuery}, Count: true, MWSIdsOnly: true}, "testdata/count_all_ids.json", false},
+		{"first 10 mws ids", &Args{Expressions: []string{MWSXMLAnyQuery}, From: 0, Size: 10, MWSIdsOnly: true}, "testdata/first_10_ids.json", false},
+		{"last 10 mws ids", &Args{Expressions: []string{MWSXMLAnyQuery}, From: 735, Size: 10, MWSIdsOnly: true}, "testdata/last_10_ids.json", false},
+
+		{"count all terms of shape x + y", &Args{Expressions: []string{MWSXMLPlusQuery}, Count: true}, "testdata/count_plus_regular.json", false},
+		{"first 10 terms of shape x + y", &Args{Expressions: []string{MWSXMLPlusQuery}, From: 0, Size: 10}, "testdata/first_10_plus_sums_regular.json", false},
+		{"last 10 terms of shape x + y", &Args{Expressions: []string{MWSXMLPlusQuery}, From: 17, Size: 10}, "testdata/last_10_plus_sums_regular.json", false},
+
+		{"count all ids of shape x + y", &Args{Expressions: []string{MWSXMLPlusQuery}, Count: true, MWSIdsOnly: true}, "testdata/count_plus_ids.json", false},
+		{"first 10 ids of shape x + y", &Args{Expressions: []string{MWSXMLPlusQuery}, MWSIdsOnly: true, From: 0, Size: 10}, "testdata/first_10_plus_sums_ids.json", false},
+		{"last 10 ids of shape x + y", &Args{Expressions: []string{MWSXMLPlusQuery}, MWSIdsOnly: true, From: 12, Size: 10}, "testdata/last_10_plus_sums_ids.json", false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
